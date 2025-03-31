9c8d9d7bea66b0ef546fae3547134777367007a3
Prasang <prasangjhawar@gmail.com>
2025-03-31 22:39:46 +0530

    Report Repository updated

--- src/main/java/org/example/repository/ReportRepository.java
+++ src/main/java/org/example/repository/ReportRepository.java
@@ -1,4 +1,7 @@
 package org.example.repository;
 
-public class ReportRepository {
+import org.example.model.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
 }
