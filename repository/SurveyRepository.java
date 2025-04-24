cdff62b8734a5a454e3507c179c9142c4d4ae6a3
PrasangJhawar <63912734+PrasangJhawar@users.noreply.github.com>
2025-04-24 19:00:57 +0530

    Delete repository directory

--- repository/SurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Survey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SurveyRepository extends JpaRepository<Survey, UUID> {
-}
